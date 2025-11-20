2f7c9f1fe39da52f32a6c6ce333367d4003c31b5
zahid-IT <zahidbilal2k15@gmail.com>
2025-11-20 11:31:42 +0500

    Update wsgi.py

--- notesapp/wsgi.py
+++ notesapp/wsgi.py
@@ -1,14 +1,6 @@
-"""
-WSGI config for notesapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
+# notesapp/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesapp.settings')
